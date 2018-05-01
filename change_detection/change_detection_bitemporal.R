b95af4ef19c1b4683bd129501b26059bffd50aab
dominiqueweber <domi.wbr@gmail.com>
2018-05-01 07:55:52 +0200

    Improved comments

--- change_detection/change_detection_bitemporal.R
+++ change_detection/change_detection_bitemporal.R
@@ -1,65 +1,63 @@
 ######################################################################
-# Map forest changes using bi-temporal Sentinel-2 differences.
+# Map forest change using bi-temporal Sentinel-2 differences.
 #
 # (C) Dominique Weber, HAFL, BFH
 ######################################################################
 
+# Load packages
 library(raster)
 library(rgdal)
 library(rgeos)
 library(ggplot2)
 
-
+# Custom change detection function
 change_detection_bitemporal <- function(date1, date2, band, aoi, threshold, out_dir, dates_text, min_area=10) {
 
+  # load Sentinel-2 image stacks
   print("Loading data...")
-  # load stacks
   r_date1 = stack(date1)
   r_date2 = stack(date2)
   
-  # load mask
+  # load mask for area of interest
   sp_mask = readOGR(aoi)
-  
+
+  # set proper band names 
+  print("Preparing data...")
   # layer names (see: https://sentinel.geodata.linux.bfh.ch/, Table2)
   BAND_NAMES = c("B01","B02","B03","B04","B05","B06","B07","B08","B8A","B09","B10","B11","B12")
-  
-  # crop
-  crop_extent = extent(sp_mask)
-  
-  print("Preparing data...")
-  # name and crop data
   names(r_date1) = BAND_NAMES
   names(r_date2) = BAND_NAMES
+  
+  # crop and mask sentinel-2 images to area of interest
+  crop_extent = extent(sp_mask)
   r_date1 = crop(r_date1, crop_extent)
   r_date2 = crop(r_date2, crop_extent)
-  
-  # clip to forest mask
   r_date1 = mask(r_date1, sp_mask)
   r_date2 = mask(r_date2, sp_mask)
   
-  # plot true colour RGBs
+  # plot true colour RGBs to PDF
   rgb_bands = c("B04", "B03", "B02")
   pdf(file=paste(out_dir, "rgb_plots.pdf", sep=""))  
   plotRGB(r_date1[[rgb_bands]], stretch="lin", axes=T, main=dates_text[1])
   plotRGB(r_date2[[rgb_bands]], stretch="lin", axes=T, main=dates_text[2])
   dev.off()
   
-  # calculate some indices
+  # calculate some vegetation indices
+  # NDII -> http://www.sciencedirect.com/science/article/pii/S0168192309002251
   r_date1$NDVI = (r_date1$B08 - r_date1$B04)/(r_date1$B08 + r_date1$B04)
   r_date1$NBR = (r_date1$B08 - r_date1$B12)/(r_date1$B08 + r_date1$B12)
-  r_date1$NDII = (r_date1$B08 - r_date1$B11)/(r_date1$B08 + r_date1$B11) # http://www.sciencedirect.com/science/article/pii/S0168192309002251
-  
+  r_date1$NDII = (r_date1$B08 - r_date1$B11)/(r_date1$B08 + r_date1$B11) 
   r_date2$NDVI = (r_date2$B08 - r_date2$B04)/(r_date2$B08 + r_date2$B04)
   r_date2$NBR = (r_date2$B08 - r_date2$B12)/(r_date2$B08 + r_date2$B12)
   r_date2$NDII = (r_date2$B08 - r_date2$B11)/(r_date2$B08 + r_date2$B11)
   # further indices tested: MSI, mNDVI705, IRECI, EVI -> they seem not usefull for forests
   # see (https://www.sentinel-hub.com/develop/documentation/eo_products/Sentinel2EOproducts)
   
-  print("Calculate differences and detect change...")
   # calculate differences
+  print("Calculate differences and detect change...")
   r_diff = r_date2 - r_date1
   
-  # plot differences within all bands and indices
+  # plot differences for all bands and indices to PDF
   pdf(file=paste(out_dir, "diff_plots.pdf", sep=""))  
   for(i in 1:nlayers(r_diff)) plot(r_diff[[i]], main=names(r_diff[[i]]))
   dev.off()
@@ -68,7 +66,7 @@ change_detection_bitemporal <- function(date1, date2, band, aoi, threshold, out_
   change_positive = r_diff[[band]] > threshold
   change_negative = r_diff[[band]] < -threshold
 
-  # apply median filter
+  # apply median filter (to remove isolated pixels)
   change_positive_3x3 = focal(change_positive, w=matrix(1,3,3), fun=modal, na.rm=T)
   change_negative_3x3 = focal(change_negative, w=matrix(1,3,3), fun=modal, na.rm=T)
 
@@ -76,8 +74,8 @@ change_detection_bitemporal <- function(date1, date2, band, aoi, threshold, out_
   change_positive_3x3 = mask(change_positive_3x3, sp_mask)
   change_negative_3x3 = mask(change_negative_3x3, sp_mask)
 
-  print("Create shapefile...")
   # create polygons for changed areas
+  print("Create shapefile...")
   change_positive_poly = rasterToPolygons(change_positive_3x3, fun=function(x){x>0}, dissolve = T)
   change_negative_poly = rasterToPolygons(change_negative_3x3, fun=function(x){x>0}, dissolve = T)
   changed_areas <- rbind(change_positive_poly, change_negative_poly)
@@ -85,14 +83,14 @@ change_detection_bitemporal <- function(date1, date2, band, aoi, threshold, out_
   changed_areas@data$change[2] = "negative"
   changed_areas@data$change = as.factor(changed_areas@data$change)
   
-  # disaggregate and mean change per polygon
+  # disaggregate and calculate mean change per polygon
   changed_areas = disaggregate(changed_areas)
   changed_areas$area_m2 = area(changed_areas)
   changed_areas = changed_areas[changed_areas$area_m2 >= min_area, ]
   changed_areas$mean = as.numeric(extract(r_diff[[band]], changed_areas, mean))
 
-  print("Final plot and write data...")
   # plot to PDF
+  print("Final plot and write data...")
   pdf(file=paste(out_dir, "changed_area.pdf", sep=""))  
   plotRGB(r_date2[[rgb_bands]], stretch="lin", axes=T, main=paste("Changed area ", dates_text[1], " - ", dates_text[2], " (", sum(changed_areas$area_m2), " m2)", sep=""))
   plot(changed_areas, col=c("red","green")[as.numeric(changed_areas$change)], border=NA, add=T)
