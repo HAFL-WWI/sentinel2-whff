29d817e7a95899a4ed927c926ce41c1b0a2b77ff
dominiqueweber <domi.wbr@gmail.com>
2018-07-10 12:27:06 +0200

    BAP composite function (not working yet)

--- composites/calc_pixel_composites.R
+++ composites/calc_pixel_composites.R
@@ -53,3 +53,35 @@ calc_pixel_composites <- function(stack_path, band_names, dates, txt, stack_fun,
   stopCluster(cl)
   return(pixel_composite)
 }
+
+# TODO not working yet...
+# BAP composite function
+# the idea is to composite the entire stack (band 1-13) based on i.e. NDVI max
+calc_bap_composite <- function(stack_path, band_names, quality_stack, quality_band) {
+  # load packages
+  library(raster)
+  library(rgdal)
+  
+  # filter .tif files and dates
+  fileNames = list.files(stack_path)
+  fileNames = fileNames[grep("tif$", fileNames)]
+  fileNames = fileNames[grepl(paste(dates, collapse="|"), fileNames)]
+  fileNames = paste(stack_path, fileNames, sep="")
+  
+  # init stack
+  bap_composite = stack(fileNames[1])
+  names(bap_composite) = band_names
+  
+  for(i in 1:length(fileNames)){
+    # get stack
+    stk_tmp = stack(fileNames[i])
+    names(stk_tmp) = band_names
+    
+    # add masked quality pixels to bap composite
+    quality_mask = (quality_stack[i] == quality_band)
+    for(i in 1:nlayers(bap_composite)){
+      bap_composite[i][quality_mask] = stk_tmp[i][quality_mask]
+    }
+  }
+  return(bapcomposite)
+}
